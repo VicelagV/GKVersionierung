076dd6fd5af21b204168cd30dc6cd63ee4462086
Luca <deine@email.at>
2026-09-16 13:34:36 +0200

    Model angelegt und Gitignore angepasst

---
+++ src/model/GewinnModel.java
@@ -0,0 +1,19 @@
+package model;
+public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
+    public GewinnModel() {
+        gesamtPunkte = 30;
+    }
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
+}
